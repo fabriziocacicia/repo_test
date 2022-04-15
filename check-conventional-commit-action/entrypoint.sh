54b9484152d6cafb48d3623fa4e6111886e204dd
Fabrizio Cacicia <mail@fabriziocacicia.com>
2022-04-15 22:04:44 +0200

    Update entrypoint.sh

--- check-conventional-commit-action/entrypoint.sh
+++ check-conventional-commit-action/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -l
-sh -c "npx commitlint -h"
-sh -c "echo HEAD"
-sh -c "echo $HEAD"
-sh -c "npx commitlint --from HEAD~1 --to HEAD --verbose"
+
+echo HEAD
+echo $HEAD
+
+npx commitlint --from HEAD~1 --to HEAD --verbose

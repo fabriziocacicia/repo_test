8f6a79007018f49c532f78fabe29642259adb6ed
Fabrizio Cacicia <mail@fabriziocacicia.com>
2022-04-16 19:31:45 +0200

    Update create_pr.sh

--- keep-history-conventional/create_pr.sh
+++ keep-history-conventional/create_pr.sh
@@ -3,6 +3,8 @@
 cd "${GITHUB_WORKSPACE}"
 
 git config --global --add safe.directory /github/workspace
+git config --global user.email "keep-history-conventional-action[bot]@users.noreply.github.com"
+git config --global user.name "Keep History Conventional Action"
 
 LATEST_COMMIT_HASH=$(git rev-parse HEAD)
 LATEST_COMMIT_HASH=$(git log -1 --format=%h)

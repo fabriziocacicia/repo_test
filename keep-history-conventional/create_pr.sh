b36550e6bafe86c929570aa088ce4afbb1b42c37
Fabrizio Cacicia <mail@fabriziocacicia.com>
2022-04-16 18:10:10 +0200

    Create create_pr.sh

---
+++ keep-history-conventional/create_pr.sh
@@ -0,0 +1,15 @@
+!/bin/sh -l
+
+cd "${GITHUB_WORKSPACE}"
+
+git config --global --add safe.directory /github/workspace
+
+LATEST_COMMIT_HASH=$(git rev-parse HEAD)
+LATEST_COMMIT_HASH=$(git log -1 --format=%h)
+NEW_BRANCH_NAME="keep_history_conventional/$LATEST_COMMIT_HASH"
+
+COMMIT_MESSAGE=$(git log -1 --format=%B)
+
+git checkout -b $NEW_BRANCH_NAME
+git commit -a -m $COMMIT_MESSAGE
+git push

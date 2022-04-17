eaea12e06bd57f2098ae7d61fe3e5cde74c3e0ad
Fabrizio Cacicia <mail@fabriziocacicia.com>
2022-04-18 00:55:48 +0200

    ci: update prepare_pr.sh

--- keep-history-conventional/prepare_pr.sh
+++ keep-history-conventional/prepare_pr.sh
@@ -17,7 +17,9 @@ cd "${GITHUB_WORKSPACE}"
 git config --global --add safe.directory /github/workspace
 
 git fetch --all
+git status
 git checkout -b $NEW_BRANCH_NAME
+git status
 #git add --all
-#git push --set-upstream origin $NEW_BRANCH_NAME
+git push --set-upstream origin $NEW_BRANCH_NAME
 

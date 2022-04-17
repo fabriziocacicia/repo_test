a8e627526cae4dd08b1fe7cc47353480aa798402
Fabrizio Cacicia <mail@fabriziocacicia.com>
2022-04-17 13:03:02 +0200

    Update and rename create_pr.sh to prepare_pr.sh

--- keep-history-conventional/prepare_pr.sh
+++ keep-history-conventional/prepare_pr.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 
+while getopts b: flag
+do
+    case "${flag}" in
+        f) NEW_BRANCH_NAME=${OPTARG};;
+    esac
+done
+
+if [ -z "$NEW_BRANCH_NAME" ]; then
+    echo "Missing new branch name. Exiting..."
+    exit
+fi
+
 cd "${GITHUB_WORKSPACE}"
 
 git config --global --add safe.directory /github/workspace
 git config --global user.email "keep-history-conventional-action[bot]@users.noreply.github.com"
 git config --global user.name "Keep History Conventional Action"
-
-LATEST_COMMIT_HASH=$(git log -1 --format=%h)
-NEW_BRANCH_NAME="keep_history_conventional/$LATEST_COMMIT_HASH"
-
-COMMIT_MESSAGE=$(git log -1 --format=%B)
 
 git fetch --all
 git checkout -b $NEW_BRANCH_NAME
